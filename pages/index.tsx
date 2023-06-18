2d8ff4c0ac139bfc07c44500af1eb3b1614a4f7c
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-06-18 02:27:56 -0700

    UI done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,41 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import React from "react";
+import { PlusCircleIcon, MinusCircleIcon } from "@heroicons/react/24/outline";
+import Button from "@/common/components/Button";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+interface ISecret {
+  key: string;
+  value: string;
+}
+
+interface IPayload {
+  key: string;
+  value: string;
+}
+
+interface IErrMsg {
+  statusCode: string;
+  msg: string;
+}
 
 export default function Home() {
+  const [secrets, setSecrets] = React.useState<ISecret[]>([]);
+  const [payloads, setPayload] = React.useState<IPayload[]>([]);
+  const [errMsg, setErrMsg] = React.useState<IErrMsg[]>([]);
+  const [phoneNum, setPhoneNum] = React.useState<string>("");
+
+  React.useEffect(() => {
+    console.log(secrets);
+  }, [secrets]);
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      {/* LOGO & HERO */}
+      <div className="mt-24 mb-24 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -43,76 +45,219 @@ export default function Home() {
           priority
         />
       </div>
+      {/* PHONE NUMBER */}
+      {phoneNum && (
+        <div className="flex flex-col gap-2 items-center justify-center w-1/2 border border-gray-700 py-12 rounded-3xl mb-12">
+          <h4 className="text-gray-300">
+            Here's the SMS number to chat with your model
+          </h4>
+          <h1>(504) 761-3416</h1>
+        </div>
+      )}
+      {/* API FORM */}
+      <div className="flex flex-col gap-12 w-1/2 items-center">
+        <input
+          type="text"
+          placeholder="Your API route"
+          className="w-full bg-transparent border-2 border-gray-800 text-3xl font-medium tracking-tight px-6 py-4 rounded-2xl"
+        ></input>
+        <p className="text-lg text-gray-400">
+          Set up your headers, body and custom error messages to expose the API
+          to an SMS service
+        </p>
+        {/* REQUEST HEADERS */}
+        <div className="w-full flex flex-col gap-4">
+          <div className="w-full flex flex-row justify-between items-center">
+            <h3>Request Headers</h3>
+            <button
+              onClick={() => {
+                const updatedSecrets = [...secrets, { key: "", value: "" }];
+                setSecrets(updatedSecrets);
+              }}
+            >
+              <PlusCircleIcon className="w-10 h-10 text-gray-500" />
+            </button>
+          </div>
+          <div className="w-full flex flex-col gap-3">
+            {secrets.map((secret, index) => {
+              return (
+                <div
+                  className="w-full flex flex-row items-center gap-2"
+                  key={index}
+                >
+                  <input
+                    type="text"
+                    placeholder="Key"
+                    defaultValue={secret.key}
+                    onChange={(e) => {
+                      const updatedSecrets = [...secrets];
+                      updatedSecrets[index] = {
+                        ...updatedSecrets[index],
+                        key: e.target.value,
+                      };
+                      setSecrets(updatedSecrets);
+                    }}
+                    className="w-2/5 bg-transparent border-2 border-gray-800 text-2xl font-numeric tracking-tight px-5 py-3 rounded-xl"
+                  ></input>
+                  <input
+                    type="Value"
+                    placeholder="value"
+                    defaultValue={secret.value}
+                    className="grow bg-transparent border-2 border-gray-800 text-2xl font-numeric tracking-tight px-5 py-3 rounded-xl"
+                    onChange={(e) => {
+                      const updatedSecrets = [...secrets];
+                      updatedSecrets[index] = {
+                        ...updatedSecrets[index],
+                        value: e.target.value,
+                      };
+                      setSecrets(updatedSecrets);
+                    }}
+                  ></input>
+                  <button
+                    onClick={() => {
+                      const updatedSecrets = [...secrets]; // Create a copy of the original array
+                      updatedSecrets.splice(index, 1); // Remove the element at the specified index
+                      setSecrets(updatedSecrets); // Update the state with the new array
+                    }}
+                  >
+                    <MinusCircleIcon className="w-8 h-8 text-gray-500" />
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        {/* REQUEST BODY */}
+        <div className="w-full flex flex-col gap-4">
+          <div className="w-full flex flex-row justify-between items-center">
+            <h3>Request Body</h3>
+            <button
+              onClick={() => {
+                const updatedPayload = [...payloads, { key: "", value: "" }];
+                setPayload(updatedPayload);
+              }}
+            >
+              <PlusCircleIcon className="w-10 h-10 text-gray-500" />
+            </button>
+          </div>
+          <div className="w-full flex flex-col gap-3">
+            {payloads.map((payload, index) => {
+              return (
+                <div
+                  className="w-full flex flex-row items-center gap-2"
+                  key={index}
+                >
+                  <input
+                    type="text"
+                    placeholder="Key"
+                    defaultValue={payload.key}
+                    onChange={(e) => {
+                      const updatedPayload = [...payloads];
+                      updatedPayload[index] = {
+                        ...updatedPayload[index],
+                        key: e.target.value,
+                      };
+                      setPayload(updatedPayload);
+                    }}
+                    className="w-2/5 bg-transparent border-2 border-gray-800 text-2xl font-numeric tracking-tight px-5 py-3 rounded-xl"
+                  ></input>
+                  <input
+                    type="Value"
+                    placeholder="value"
+                    defaultValue={payload.value}
+                    className="grow bg-transparent border-2 border-gray-800 text-2xl font-numeric tracking-tight px-5 py-3 rounded-xl"
+                    onChange={(e) => {
+                      const updatedPayload = [...payloads];
+                      updatedPayload[index] = {
+                        ...updatedPayload[index],
+                        value: e.target.value,
+                      };
+                      setPayload(updatedPayload);
+                    }}
+                  ></input>
+                  <button
+                    onClick={() => {
+                      const updatedPayload = [...payloads]; // Create a copy of the original array
+                      updatedPayload.splice(index, 1); // Remove the element at the specified index
+                      setPayload(updatedPayload); // Update the state with the new array
+                    }}
+                  >
+                    <MinusCircleIcon className="w-8 h-8 text-gray-500" />
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        {/* ERROR MESSAGE */}
+        <div className="w-full flex flex-col gap-4">
+          <div className="w-full flex flex-row justify-between items-center">
+            <h3>Error Message</h3>
+            <button
+              onClick={() => {
+                const updatedErrMsg = [...errMsg, { statusCode: "", msg: "" }];
+                setErrMsg(updatedErrMsg);
+              }}
+            >
+              <PlusCircleIcon className="w-10 h-10 text-gray-500" />
+            </button>
+          </div>
+          <div className="w-full flex flex-col gap-3">
+            {errMsg.map((err, index) => {
+              return (
+                <div
+                  className="w-full flex flex-row items-center gap-2"
+                  key={index}
+                >
+                  <input
+                    type="text"
+                    placeholder="Status Code"
+                    defaultValue={err.statusCode}
+                    onChange={(e) => {
+                      const updatedErrMsg = [...errMsg];
+                      updatedErrMsg[index] = {
+                        ...updatedErrMsg[index],
+                        statusCode: e.target.value,
+                      };
+                      setErrMsg(updatedErrMsg);
+                    }}
+                    className="w-2/5 bg-transparent border-2 border-gray-800 text-2xl font-numeric tracking-tight px-5 py-3 rounded-xl"
+                  ></input>
+                  <input
+                    type="Value"
+                    placeholder="value"
+                    defaultValue={err.msg}
+                    className="grow bg-transparent border-2 border-gray-800 text-2xl font-numeric tracking-tight px-5 py-3 rounded-xl"
+                    onChange={(e) => {
+                      const updatedErrMsg = [...errMsg];
+                      updatedErrMsg[index] = {
+                        ...updatedErrMsg[index],
+                        msg: e.target.value,
+                      };
+                      setErrMsg(updatedErrMsg);
+                    }}
+                  ></input>
+                  <button
+                    onClick={() => {
+                      const updatedErrMsg = [...errMsg]; // Create a copy of the original array
+                      updatedErrMsg.splice(index, 1); // Remove the element at the specified index
+                      setErrMsg(updatedErrMsg); // Update the state with the new array
+                    }}
+                  >
+                    <MinusCircleIcon className="w-8 h-8 text-gray-500" />
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <Button>
+          <h3>Create SMS Number</h3>
+        </Button>
       </div>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,46 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './common/components/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    fontFamily: {
+      'sans': ['"Work Sans"', ...defaultTheme.fontFamily.sans],
+    },
     extend: {
+      fontFamily: {
+        'numeric': ['"DM Mono"', ...defaultTheme.fontFamily.mono],
+        'satoshi': ['Satoshi', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'theme' : {
+          5: '#0C0C0C',
+          15: '#262626',
+          20: '#333333',
+          25: '#404040', 
+          35: '#595959',
+          45: '#737272',
+          55: '#8D8C8C',
+          65: '#A6A6A6',
+          75: '#BFBFBF',
+          85: '#D9D9D9',
+          90: '#E5E5E5',
+          95: '#F2F2F2',
+        },
+        'accent' : {
+          60: '#D5E151',
+        },
+        'warning' : {
+          'bg': "rgba(247,13,13, 0.2)",
+          'border': "rgba(247,13,13, 0.06)",
+          'text': "#F75252",
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
