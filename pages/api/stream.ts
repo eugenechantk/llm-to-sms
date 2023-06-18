d2438cb39002f983d4bb44ea2df93c98dbd319fb
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-06-17 19:13:01 -0700

    remove axios

--- pages/api/stream.ts
+++ pages/api/stream.ts
@@ -1,5 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import axios, { AxiosError } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
