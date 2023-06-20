c412eaa4eae0352bf20c7664b7946201e5d8cdb6
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-06-19 23:50:13 -0700

    responsive design for portal

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { PlusCircleIcon, MinusCircleIcon } from "@heroicons/react/24/outline";
 import Button from "@/common/components/Button";
 import axios from "axios";
 import Logo from "../public/logo.svg";
+import NoWifiIcon from "../public/nowifi.svg";
 const inter = Inter({ subsets: ["latin"] });
 
 interface ISecret {
@@ -22,6 +23,10 @@ interface IErrMsg {
   msg: string;
 }
 
+const baseUrl = process.env.VERCEL_URL
+  ? "https://" + process.env.VERCEL_URL
+  : "http://localhost:3000";
+
 export default function Home() {
   const [secrets, setSecrets] = React.useState<ISecret[]>([]);
   const [payloads, setPayload] = React.useState<IPayload[]>([]);
@@ -32,10 +37,12 @@ export default function Home() {
   const handlePhoneNumberProvision = async () => {
     setLoading(true)
     try {
-      let service = (await axios.post('http://localhost:3000/api/twilio/setup/start', {
-        SERVER: 'https://llm-to-sms.vercel.app',
-        uuid: 'a1-11'
-      })).data
+      let service = (
+        await axios.post(`${baseUrl}/api/twilio/setup/start`, {
+          SERVER: "https://llm-to-sms.vercel.app",
+          uuid: "a1-10",
+        })
+      ).data;
       console.log(service.number);
      setPhoneNum(service.number)
      setLoading(false)
@@ -49,251 +56,78 @@ export default function Home() {
   }, [secrets]);
   return (
     <main
-      className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center p-6 ${inter.className}`}
     >
       {/* LOGO & HERO */}
-      <div className="mt-24 mb-24 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] flex-col gap-10">
+      <div className="mt-12 mb-16 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] flex-col gap-10">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src={Logo}
-          alt="Next.js Logo"
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+          src={NoWifiIcon}
+          alt="Offline SMS logo"
           width={120}
           height={37}
           priority
         />
         <div className="flex flex-col gap-6 items-center">
-        <h3 className="text-gray-300 text-5xl">Offline GPT</h3>
-        <p className="text-3xl text-white-500">A framework and gateway to reaching an extra 2.9 billion </p>
-        <p className="text-3xl text-gray-500">Deploy your LLM product and serve your chat model via SMS</p>
+          <div className="flex flex-col gap-6 items-center">
+            <h3 className="text-gray-100 text-5xl">Offline GPT</h3>
+            <p className="text-3xl text-gray-200 text-center">
+              A gateway framework to reaching an extra 2.9 billion people
+            </p>
+            <p className="text-3xl text-gray-500 text-center">
+              Deploy your LLM product and serve your chat model via SMS
+            </p>
+          </div>
         </div>
       </div>
       {/* PHONE NUMBER */}
       {phoneNum && (
-        <div className="flex flex-col gap-2 items-center justify-center w-1/2 border border-gray-700 py-12 rounded-3xl mb-12">
-          <h4 className="text-gray-300">
+        <div className="flex flex-col gap-2 items-center justify-center w-full lg:max-w-[640px] lg:w-full border border-gray-700 rounded-3xl mb-12 p-12">
+          <h4 className="text-gray-300 text-center">
             Here&apos;s the SMS number to chat with your model
           </h4>
           <h1>{phoneNum}</h1>
         </div>
       )}
       {/* API FORM */}
-      <div className="flex flex-col gap-12 w-1/2 items-center">
-        <input
-          type="text"
-          placeholder="Your API route"
-          className="w-full bg-transparent border-2 border-gray-800 text-3xl font-medium tracking-tight px-6 py-4 rounded-2xl"
-        ></input>
-        <button style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "16px 20px",
-        gap: "4px",
-        background: "linear-gradient(0deg, #262626 63.89%, #333333 100%)",
-        border: "0.75px solid #595959",
-      }}
-      className={
-        "rounded-full text-sm font-semibold hover:!bg-gradient-to-b hover:!from-theme-15 hover:!from-[64%] hover:!to-theme-25 hover:!to-[100%] w-[320px]"
-      }onClick={() => handlePhoneNumberProvision() }>
-          {loading ? <h3 className="text-gray-200">Loading...</h3>: <h3>Create SMS Number</h3>}
-        </button>
-        <p className="text-lg text-gray-400">
-          Set up your headers, body and custom error messages to expose the API
-          to an SMS service
-        </p>
+      <div className="flex flex-col gap-12 w-full lg:w-1/2 items-center">
+        <div className="w-full flex flex-col gap-6 items-center">
+          <input
+            type="text"
+            placeholder="Your API route"
+            className="w-full bg-transparent border-2 border-gray-800 text-3xl font-medium tracking-tight px-6 py-4 rounded-2xl"
+          ></input>
+          <button
+            onClick={() => handlePhoneNumberProvision()}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "16px 20px",
+              gap: "4px",
+              background: "linear-gradient(0deg, #262626 63.89%, #333333 100%)",
+              border: "0.75px solid #595959",
+              width: "fit-content",
+            }}
+            className="rounded-full text-sm font-semibold hover:!bg-gradient-to-b hover:!from-theme-15 hover:!from-[64%] hover:!to-theme-25 hover:!to-[100%]"
+          >
+            <h3>Create SMS Number</h3>
+          </button>
+        </div>
+
         {/* REQUEST HEADERS */}
         <div className="w-full flex flex-col gap-4">
-          <div className="w-full flex flex-row justify-between items-center">
-            <h3>Request Headers</h3>
-            <button
-              onClick={() => {
-                const updatedSecrets = [...secrets, { key: "", value: "" }];
-                setSecrets(updatedSecrets);
-              }}
-            >
-              <PlusCircleIcon className="w-10 h-10 text-gray-500" />
-            </button>
-          </div>
-          <div className="w-full flex flex-col gap-3">
-            {secrets.map((secret, index) => {
-              return (
-                <div
-                  className="w-full flex flex-row items-center gap-2"
-                  key={index}
-                >
-                  <input
-                    type="text"
-                    placeholder="Key"
-                    defaultValue={secret.key}
-                    onChange={(e) => {
-                      const updatedSecrets = [...secrets];
-                      updatedSecrets[index] = {
-                        ...updatedSecrets[index],
-                        key: e.target.value,
-                      };
-                      setSecrets(updatedSecrets);
-                    }}
-                    className="w-2/5 bg-transparent border-2 border-gray-800 text-2xl font-numeric tracking-tight px-5 py-3 rounded-xl"
-                  ></input>
-                  <input
-                    type="Value"
-                    placeholder="value"
-                    defaultValue={secret.value}
-                    className="grow bg-transparent border-2 border-gray-800 text-2xl font-numeric tracking-tight px-5 py-3 rounded-xl"
-                    onChange={(e) => {
-                      const updatedSecrets = [...secrets];
-                      updatedSecrets[index] = {
-                        ...updatedSecrets[index],
-                        value: e.target.value,
-                      };
-                      setSecrets(updatedSecrets);
-                    }}
-                  ></input>
-                  <button
-                    onClick={() => {
-                      const updatedSecrets = [...secrets]; // Create a copy of the original array
-                      updatedSecrets.splice(index, 1); // Remove the element at the specified index
-                      setSecrets(updatedSecrets); // Update the state with the new array
-                    }}
-                  >
-                    <MinusCircleIcon className="w-8 h-8 text-gray-500" />
-                  </button>
-                </div>
-              );
-            })}
-          </div>
+          <h3>Request Parameters</h3>
+          <p className="text-lg text-gray-400">
+            Set up your headers, body and custom error messages to expose the
+            API to an SMS service
+          </p>
+          <textarea
+            className="w-full bg-transparent border-2 border-gray-800 text-xl font-medium tracking-tight px-6 py-4 rounded-2xl h-[280px]"
+            placeholder="Specify your request headers, body and other settings"
+          ></textarea>
         </div>
-
-        {/* REQUEST BODY */}
-        <div className="w-full flex flex-col gap-4">
-          <div className="w-full flex flex-row justify-between items-center">
-            <h3>Request Body</h3>
-            <button
-              onClick={() => {
-                const updatedPayload = [...payloads, { key: "", value: "" }];
-                setPayload(updatedPayload);
-              }}
-            >
-              <PlusCircleIcon className="w-10 h-10 text-gray-500" />
-            </button>
-          </div>
-          <div className="w-full flex flex-col gap-3">
-            {payloads.map((payload, index) => {
-              return (
-                <div
-                  className="w-full flex flex-row items-center gap-2"
-                  key={index}
-                >
-                  <input
-                    type="text"
-                    placeholder="Key"
-                    defaultValue={payload.key}
-                    onChange={(e) => {
-                      const updatedPayload = [...payloads];
-                      updatedPayload[index] = {
-                        ...updatedPayload[index],
-                        key: e.target.value,
-                      };
-                      setPayload(updatedPayload);
-                    }}
-                    className="w-2/5 bg-transparent border-2 border-gray-800 text-2xl font-numeric tracking-tight px-5 py-3 rounded-xl"
-                  ></input>
-                  <input
-                    type="Value"
-                    placeholder="value"
-                    defaultValue={payload.value}
-                    className="grow bg-transparent border-2 border-gray-800 text-2xl font-numeric tracking-tight px-5 py-3 rounded-xl"
-                    onChange={(e) => {
-                      const updatedPayload = [...payloads];
-                      updatedPayload[index] = {
-                        ...updatedPayload[index],
-                        value: e.target.value,
-                      };
-                      setPayload(updatedPayload);
-                    }}
-                  ></input>
-                  <button
-                    onClick={() => {
-                      const updatedPayload = [...payloads]; // Create a copy of the original array
-                      updatedPayload.splice(index, 1); // Remove the element at the specified index
-                      setPayload(updatedPayload); // Update the state with the new array
-                    }}
-                  >
-                    <MinusCircleIcon className="w-8 h-8 text-gray-500" />
-                  </button>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-
-        {/* ERROR MESSAGE */}
-        <div className="w-full flex flex-col gap-4">
-          <div className="w-full flex flex-row justify-between items-center">
-            <h3>Error Message</h3>
-            <button
-              onClick={() => {
-                const updatedErrMsg = [...errMsg, { statusCode: "", msg: "" }];
-                setErrMsg(updatedErrMsg);
-              }}
-            >
-              <PlusCircleIcon className="w-10 h-10 text-gray-500" />
-            </button>
-          </div>
-          <div className="w-full flex flex-col gap-3">
-            {errMsg.map((err, index) => {
-              return (
-                <div
-                  className="w-full flex flex-row items-center gap-2"
-                  key={index}
-                >
-                  <input
-                    type="text"
-                    placeholder="Status Code"
-                    defaultValue={err.statusCode}
-                    onChange={(e) => {
-                      const updatedErrMsg = [...errMsg];
-                      updatedErrMsg[index] = {
-                        ...updatedErrMsg[index],
-                        statusCode: e.target.value,
-                      };
-                      setErrMsg(updatedErrMsg);
-                    }}
-                    className="w-2/5 bg-transparent border-2 border-gray-800 text-2xl font-numeric tracking-tight px-5 py-3 rounded-xl"
-                  ></input>
-                  <input
-                    type="Value"
-                    placeholder="value"
-                    defaultValue={err.msg}
-                    className="grow bg-transparent border-2 border-gray-800 text-2xl font-numeric tracking-tight px-5 py-3 rounded-xl"
-                    onChange={(e) => {
-                      const updatedErrMsg = [...errMsg];
-                      updatedErrMsg[index] = {
-                        ...updatedErrMsg[index],
-                        msg: e.target.value,
-                      };
-                      setErrMsg(updatedErrMsg);
-                    }}
-                  ></input>
-                  <button
-                    onClick={() => {
-                      const updatedErrMsg = [...errMsg]; // Create a copy of the original array
-                      updatedErrMsg.splice(index, 1); // Remove the element at the specified index
-                      setErrMsg(updatedErrMsg); // Update the state with the new array
-                    }}
-                  >
-                    <MinusCircleIcon className="w-8 h-8 text-gray-500" />
-                  </button>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-
-        <button onClick={() => handlePhoneNumberProvision()}>
-          <h3>Create SMS Number</h3>
-        </button>
       </div>
     </main>
   );
